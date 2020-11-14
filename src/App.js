9b67955aed2e064694f16d5a3a0ce52310c779a2
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-15 00:03:13 +0500

    Update App.js
    Testing COVID API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 import Cards from './components/Cards';
+import { getData } from './api/covidData';
 
 function App() {
+	getData(
+		`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`,
+		`confirmed`,
+		(dataNew) => {
+			console.log(dataNew);
+			console.log(Object.keys(dataNew));
+		}
+	);
 	return (
 		<div className="container">
 			<Cards />
