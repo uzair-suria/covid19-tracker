a33c31dbfbcd157b5f12cabe4160db12c58a3985
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-17 00:10:00 +0500

    Update Cards.js
    - Added data context
    - Added count up functionality
    - updated css rules
    - Added Covid Values Dynamically linked  with covidContext

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,6 +6,8 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { red, indigo, green, orange, grey } from '@material-ui/core/colors';
+import { covidContext } from '../context/CovidDataStore';
+import CountUp from 'react-countup';
 
 import classNames from 'classnames';
 
@@ -20,37 +22,45 @@ const baseCard = makeStyles({
 		display: 'flex',
 		alignItems: 'center',
 		justifyContent: 'center',
-		// flexDirection: 'column',
+		flexDirection: 'row',
 	},
 });
 
 const deathCard = makeStyles({
 	root: {
-		borderBottom: `5px solid ${red[500]}`,
+		borderBottom: `10px solid ${red[500]}`,
 	},
 });
 
 const infectedCard = makeStyles({
 	root: {
-		borderBottom: `5px solid ${indigo[500]}`,
+		borderBottom: `10px solid ${indigo[500]}`,
 	},
 });
 
 const recoveredCard = makeStyles({
 	root: {
-		borderBottom: `5px solid ${green[500]}`,
+		borderBottom: `10px solid ${green[500]}`,
 	},
 });
 
 const activeCard = makeStyles({
 	root: {
-		borderBottom: `5px solid ${orange[200]}`,
+		borderBottom: `10px solid ${orange[200]}`,
 	},
 });
 
 const useStyles = makeStyles({
 	title: {
 		color: grey[700],
+		textAlign: 'center',
+	},
+	paper: {
+		backgroundColor: grey[100],
+	},
+	loadingMessage: {
+		color: grey[900],
+		textAlign: 'center',
 	},
 });
 
@@ -62,30 +72,70 @@ const Cards = () => {
 	const activeClass = activeCard();
 	const classes = useStyles();
 
-	return (
-		<Paper>
-			<Typography variant="h4" className={classes.title}>
-				Global Cases
-			</Typography>
-			<div className={baseClass.container}>
-				<Card className={classNames(baseClass.root, infectedClass.root)}>
-					<CardContent>
-						<Typography variant="body1">Infected</Typography>
-						<Typography variant="h6">53M</Typography>
-					</CardContent>
-				</Card>
-				<Card className={classNames(baseClass.root, recoveredClass.root)}>
-					<CardContent>Recovered</CardContent>
-				</Card>
-				<Card className={classNames(baseClass.root, activeClass.root)}>
-					<CardContent>Active</CardContent>
-				</Card>
-				<Card className={classNames(baseClass.root, deathClass.root)}>
-					<CardContent>Deaths</CardContent>
-				</Card>
-			</div>
-		</Paper>
+	const { country, confirmed, recovered, deaths, dates } = React.useContext(
+		covidContext
 	);
+
+	// console.log(
+	// 	`Selected country is ${country} and the data is`,
+	// 	confirmed[country].confirmed
+	// );
+
+	if (confirmed[country] && recovered[country] && deaths[country]) {
+		return (
+			<Paper className={classes.paper}>
+				<Typography variant="h5" className={classes.title}>
+					{country === 'global' ? 'Worldwide' : country} Report
+				</Typography>
+				<hr />
+				<div className={baseClass.container}>
+					<Card className={classNames(baseClass.root, infectedClass.root)}>
+						<CardContent>
+							<Typography variant="body1">Infected</Typography>
+							<Typography variant="h6">
+								<CountUp
+									start={0}
+									end={confirmed[country].confirmed[dates.length - 1]}
+									separator=","
+								/>
+							</Typography>
+						</CardContent>
+					</Card>
+					<Card className={classNames(baseClass.root, recoveredClass.root)}>
+						<CardContent>
+							<Typography variant="body1">Recovered</Typography>
+							<Typography variant="h6">
+								<CountUp
+									start={0}
+									end={recovered[country].recovered[dates.length - 1]}
+									separator=","
+								/>
+							</Typography>
+						</CardContent>
+					</Card>
+
+					<Card className={classNames(baseClass.root, deathClass.root)}>
+						<CardContent>
+							<Typography variant="body1">Deaths</Typography>
+							<Typography variant="h6">
+								<CountUp
+									start={0}
+									end={deaths[country].deaths[dates.length - 1]}
+									separator=","
+								/>
+							</Typography>
+						</CardContent>
+					</Card>
+				</div>
+			</Paper>
+		);
+	} else {
+		return (
+			<Typography variant="h4" className={classes.loadingMessage}>
+				Loading Dashboard. Please Wait...
+			</Typography>
+		);
+	}
 };
 
 export default Cards;
