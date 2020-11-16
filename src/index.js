8465ee4276827f870bf2c34140961ed39aed54ae
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-16 12:47:44 +0500

    added CovidDataStore and wrapped App comp. with it

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CovidDataStore from './context/CovidDataStore';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<CovidDataStore>
+			<App />
+		</CovidDataStore>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
